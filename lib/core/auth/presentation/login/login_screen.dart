6968d2fc331f5ef2c790c1b08203383da20d2139
Lê Vũ Hoàng <2251120349@ut.edu.vn>
2025-01-17 21:30:08 +0700

    add SafeArea for login Screen

--- lib/core/auth/presentation/login/login_screen.dart
+++ lib/core/auth/presentation/login/login_screen.dart
@@ -11,165 +11,166 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body:
-        
-        Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
+    return SafeArea(
+      child: Scaffold(
+          body:
           Container(
-            height: MediaQuery.of(context).size.height * 0.3,
-            width: MediaQuery.of(context).size.width,
-            child: CustomPaint(
-              painter: LoginCustomPaint(),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width,
+              child: CustomPaint(
+                painter: LoginCustomPaint(),
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.1,
-            child: Image.asset('assets/images/Login_logo.png',
-                width: 30, height: 30),
-          ),
-          SizedBox(height: 20),
-          Text('Khám bệnh dễ dàng, sức khỏe vững vàng',
-              style: GoogleFonts.sansita(
-                fontSize: AppColors.subtitleSize,
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.1,
+              child: Image.asset('assets/images/Login_logo.png',
+                  width: 30, height: 30),
+            ),
+            SizedBox(height: 20),
+            Text('Khám bệnh dễ dàng, sức khỏe vững vàng',
+                style: GoogleFonts.sansita(
+                  fontSize: AppColors.subtitleSize,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                )),
+            SizedBox(height: 20),
+            Text(
+              'Sign In',
+              style: TextStyle(
+                fontSize: AppColors.tittleSize,
                 fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-              )),
-          SizedBox(height: 20),
-          Text(
-            'Sign In',
-            style: TextStyle(
-              fontSize: AppColors.tittleSize,
-              fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                labelText: 'Email',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green, width: 1.0),
-                  borderRadius: BorderRadius.circular(12.0),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  labelText: 'Email',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 1.0),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                labelText: 'Password',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green, width: 1.0),
-                  borderRadius: BorderRadius.circular(12.0),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  labelText: 'Password',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 1.0),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            padding: EdgeInsets.zero,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text.rich(
-                  TextSpan(
-                    text: 'Don\'t have an account? ',
-                    children: [
-                      TextSpan(
-                        text: 'Sign up',
-                        style: TextStyle(
-                          color: AppColors.primary,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            RouterHandle.goToRegister(context);
-                          },
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text.rich(
-                  TextSpan(
-                    text: 'Forgot password? ',
-                    children: [
-                      TextSpan(
-                        text: 'Reset password',
-                        style: TextStyle(
-                          color: AppColors.primary,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            RouterHandle.goToForgotPassword(context);
-                          },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: Text('Login',
-                  style: GoogleFonts.inter(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              padding: EdgeInsets.zero,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset('assets/icons/icons8-google.png',
-                      width: 30, height: 30),
-                  SizedBox(width: 10),
-                  Text('Sign in with Google',
-                      style: GoogleFonts.inter(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      )),
+                  Text.rich(
+                    TextSpan(
+                      text: 'Don\'t have an account? ',
+                      children: [
+                        TextSpan(
+                          text: 'Sign up',
+                          style: TextStyle(
+                            color: AppColors.primary,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              RouterHandle.goToRegister(context);
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text.rich(
+                    TextSpan(
+                      text: 'Forgot password? ',
+                      children: [
+                        TextSpan(
+                          text: 'Reset password',
+                          style: TextStyle(
+                            color: AppColors.primary,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              RouterHandle.goToForgotPassword(context);
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ),
-          Spacer(),
-        ],
-      ),
-    ));
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: Text('Login',
+                    style: GoogleFonts.inter(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/icons/icons8-google.png',
+                        width: 30, height: 30),
+                    SizedBox(width: 10),
+                    Text('Sign in with Google',
+                        style: GoogleFonts.inter(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+      )),
+    );
   }
 }
